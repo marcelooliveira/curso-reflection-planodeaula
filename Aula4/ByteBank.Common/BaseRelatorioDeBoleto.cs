477b2adc248330d2a424edd3893c0f7cf01c428d
marcelooliveira <mclricardo@gmail.com>
2024-01-21 15:24:25 -0300

    Aula4 e Aula5

---
+++ Aula4/ByteBank.Common/BaseRelatorioDeBoleto.cs
@@ -0,0 +1,48 @@
+﻿namespace ByteBank.Common
+{
+    public abstract class BaseRelatorioDeBoleto : IRelatorioDeBoleto<Boleto>
+    {
+        public void SalvarBoletosPorCedente(List<Boleto> boletos)
+        {
+            var boletosPorCedenteList = PegaBoletosAgrupados(boletos);
+
+            GravarArquivo(boletosPorCedenteList);
+        }
+        private List<BoletosPorCedente> PegaBoletosAgrupados(List<Boleto> boletos)
+        {
+            // Agrupar boletos por cedente
+            var boletosAgrupados = boletos.GroupBy(b => new
+            {
+                b.CedenteNome,
+                b.CedenteCpfCnpj,
+                b.CedenteAgencia,
+                b.CedenteConta
+            });
+
+            // Lista para armazenar instâncias de BoletosPorCedente
+            List<BoletosPorCedente> boletosPorCedenteList = new List<BoletosPorCedente>();
+
+            // Iterar sobre os grupos de boletos por cedente
+            foreach (var grupo in boletosAgrupados)
+            {
+                // Criar instância de BoletosPorCedente
+                BoletosPorCedente boletosPorCedente = new BoletosPorCedente
+                {
+                    CedenteNome = grupo.Key.CedenteNome,
+                    CedenteCpfCnpj = grupo.Key.CedenteCpfCnpj,
+                    CedenteAgencia = grupo.Key.CedenteAgencia,
+                    CedenteConta = grupo.Key.CedenteConta,
+                    Valor = grupo.Sum(b => b.Valor),
+                    Quantidade = grupo.Count()
+                };
+
+                // Adicionar à lista
+                boletosPorCedenteList.Add(boletosPorCedente);
+            }
+
+            return boletosPorCedenteList;
+        }
+
+        protected abstract void GravarArquivo(List<BoletosPorCedente> listaObjetos);
+    }
+}
---
+++ Aula5/ByteBank.Common/BoletosPorSacado.cs
@@ -0,0 +1,13 @@
+﻿namespace ByteBank.Common
+{
+    public class BoletosPorSacado
+    {
+        // Informações do Sacado (Pagador)
+        public string SacadoNome { get; set; }
+        public string SacadoCpfCnpj { get; set; }
+        public string SacadoEndereco { get; set; }
+        // Totais
+        public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
